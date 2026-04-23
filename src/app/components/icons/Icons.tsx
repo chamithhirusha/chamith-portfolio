53bb20ed62c7f46512a727fd25648cd36235a3ce
Chamith <152796091+chamithhirusha@users.noreply.github.com>
2026-04-23 12:07:25 +0800

    Added text slider and other ui features

--- src/app/components/icons/Icons.tsx
+++ src/app/components/icons/Icons.tsx
@@ -47,3 +47,23 @@ export const ArrowDownLeftIcon = ({
     </svg>
   );
 };
+
+export const SpecialStarIcon = ({
+  className = "size-6",
+}: {
+  className?: string;
+}) => {
+  return (
+    <svg
+      viewBox="0 0 20 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M0 9.5861V7.29003L8.17647 5.62538L8.47059 9.87311L0 9.5861ZM4 18.3112L5.41176 10.5619L9.64706 11.5378L6.17647 19L4 18.3112ZM7.41177 0.918431L9.70588 0L14.1176 6.77341L10.1176 8.55287L7.41177 0.918431ZM9.29412 14.7523L11.7059 11.1934L18.1765 16.4169L16.9412 18.2538L9.29412 14.7523ZM11.8235 9.29909L18.8824 4.93656L20 6.94562L14.6471 12.5136L11.8235 9.29909Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
