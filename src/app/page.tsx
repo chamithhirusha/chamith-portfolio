895e88842f8c1586424459551e45c44ad891b750
Chamith Hirusha <152796091+chamithhirusha@users.noreply.github.com>
2026-05-22 23:00:29 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 
-import { useState } from "react";
-import TextSlider from "./components/sliders/TextSlider";
+import HomeContacts from "./components/pages/home/HomeContacts";
 import HomeHeader from "./components/pages/home/HomeHeader";
 import HomeLatestProjects from "./components/pages/home/HomeLatestProjects";
 import HomeServices from "./components/pages/home/HomeServices";
-import HomeContacts from "./components/pages/home/HomeContacts";
+import TextSlider from "./components/sliders/TextSlider";
 
 export default function Home() {
-  const [showWarning, setShowWarning] = useState(
-    process.env.NEXT_PUBLIC_DEMO_MODE === "true",
-  );
-
   return (
     <>
       <HomeHeader />
